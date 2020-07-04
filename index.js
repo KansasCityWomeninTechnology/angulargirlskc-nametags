85c2adbe28e87118d598596a63f8c6f5ba6299e5
alisaduncan <7586237+alisaduncan@users.noreply.github.com>
2020-07-04 12:48:10 -0500

    update assets and allow for arguments

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var Jimp = require('jimp');
 const readline = require('readline');
 const fs = require('fs');
 
+const TYPE = process.argv.slice(2)[0] ? process.argv.slice(2)[0] : 'attendee';
+const NAMEBADGE_FILE = `./assets/${TYPE}.png`;
+
+
 let rl = readline.createInterface({
    input: fs.createReadStream('names.txt')
 });
@@ -9,17 +13,17 @@ let rl = readline.createInterface({
 rl.on('line', (line) => {
    const fileName = line.replace(/\s+/g, '-').toLowerCase();
 
-   Jimp.read('./assets/attendee.png')
-   .then(img => Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(font => [img, font]))
+   Jimp.read(NAMEBADGE_FILE)
+   .then(img => Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font => [img, font]))
    .then(data => {
       const img = data[0];
       const font = data[1];
 
-      return img.print(font, 212, 270, {
+      return img.print(font, 112, 138, {
          text: line.toUpperCase(),
          alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
          alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
-         }, 600, 300)
+         }, 270, 140)
    })
    .then(img => img.write(`./out/${fileName}.png`))
    .catch(err => {
